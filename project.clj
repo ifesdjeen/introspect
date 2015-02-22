a36e2fed08cce91ffb69dec08ddd0d4aba74453c
Alex Petrov <oleksandr.petrov@gmail.com>
2015-02-22 22:43:46 +0100

    Formatting

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
 (defproject introspect "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description       "FIXME: write description"
+  :url               "http://example.com/FIXME"
+  :license           {:name "Eclipse Public License"
+                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :source-paths ["src/clj"]
+  :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
-  :aot [introspect.visitor]
-  ;; :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :aot               [introspect.visitor]
+   ;; :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
 
-  :manifest {"Premain-class"           "introspect.IntrospectProfilingAgent"
-             "Agent-class"             "introspect.IntrospectProfilingAgent"
-             "Can-Redefine-Classes"    "true"
-             "Can-Retransform-Classes" "true"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.ow2.asm/asm     "5.0"]
-])
+  :manifest          {"Premain-class"           "introspect.IntrospectProfilingAgent"
+                      "Agent-class"             "introspect.IntrospectProfilingAgent"
+                      "Can-Redefine-Classes"    "true"
+                      "Can-Retransform-Classes" "true"}
+
+  :dependencies      [[org.clojure/clojure "1.6.0"]
+                      [org.ow2.asm/asm     "5.0"]])
