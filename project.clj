8fc55da5278d2e5710bededa37edc823b46e0f72
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-01 19:51:06 +0100

    Cleanup project file

--- project.clj
+++ project.clj
@@ -6,14 +6,7 @@
 
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
-  :aot               [introspect.visitor]
-   ;; :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-
-
-  :manifest          {"Premain-class"           "introspect.IntrospectProfilingAgent"
-                      "Agent-class"             "introspect.IntrospectProfilingAgent"
-                      "Can-Redefine-Classes"    "true"
-                      "Can-Retransform-Classes" "true"}
 
   :dependencies      [[org.clojure/clojure "1.6.0"]
-                      [org.ow2.asm/asm     "5.0"]])
+                      [net.bytebuddy/byte-buddy-agent "0.6-SNAPSHOT"]
+                      [net.bytebuddy/byte-buddy "0.6-SNAPSHOT"]])
