fb078b35ae5fd4f0a96c8987d12f884e891018be
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-03 22:22:05 +0100

    Add premain introspection

--- src/clj/introspect/core.clj
+++ src/clj/introspect/core.clj
@@ -63,12 +63,17 @@
     )
   )
 
+(defonce introspected-namespaces (atom #{}))
 (defn introspect-namespace
   [namespace-sym]
-  (IntrospectProfilingAgent/initializeAgent (-> namespace-sym
-                                                name
-                                                (.replace "-" "_")))
-  (compile namespace-sym)
+  (when (not (get introspected-namespaces namespace-sym))
+    (println (str "Introspecting " (name namespace-sym)))
+    (swap! introspected-namespaces conj namespace-sym)
+
+    (IntrospectProfilingAgent/initializeAgent (-> namespace-sym
+                                                  name
+                                                  (.replace "-" "_")))
+    (compile namespace-sym))
   ;; v.ns.name.name.replace('.', '/').replace('-','_') + "$" + munge(v.sym.name);
   )
 
@@ -88,3 +93,21 @@
   [t1 t2]
   (instance? (class t1)
              (class t2)))
+
+
+(gen-class
+ :name    introspect.Premain
+ :methods [#^{:static true} [premain [String java.lang.instrument.Instrumentation] void]])
+
+(defn -premain
+  [agent-args instrumentation]
+  (println " _       _                                 _ ")
+  (println "(_)_ __ | |_ _ __ ___  ___ _ __   ___  ___| |_")
+  (println "| | '_ \\| __| '__/ _ \\/ __| '_ \\ / _ \\/ __| __|")
+  (println "| | | | | |_| | | (_) \\__ \\ |_) |  __/ (__| |_")
+  (println "|_|_| |_|\\__|_|  \\___/|___/ .__/ \\___|\\___|\\__|")
+  (println "                          |_|                  ")
+
+  (binding [*compile-path* (str (System/getProperty "user.dir") "/target/classes")]
+    (doseq [n (clojure.string/split agent-args #";")]
+      (introspect-namespace (symbol n)))))
