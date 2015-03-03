ffb8de2cd18c0b0f65380d55c346a973dffd6859
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-03 22:21:31 +0100

    Add simple test

--- test/clj/introspect/core_test.clj
+++ test/clj/introspect/core_test.clj
@@ -1,12 +1,13 @@
 (ns introspect.core-test
   (:require [clojure.test :refer :all]
             [introspect.core :refer :all]
-
             [introspect.helpers.simple-functions :as h1]))
 
 (deftest a-test
-  (introspect-namespace 'introspect.helpers.simple-functions)
-
   (h1/constant-return-value-fn 1 1)
   (h1/constant-return-value-fn "1" "1")
-  )
+
+  (is (= "(java.lang.Long -> java.lang.Long -> java.lang.Long)
+(java.lang.Long -> java.lang.String -> java.lang.String)\n"
+         (with-out-str
+           (t h1/constant-return-value-fn)))))
