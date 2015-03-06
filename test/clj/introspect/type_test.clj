0d09fd00b86851486fd7f3166cc4201fd024eae8
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-06 23:34:20 +0100

    Add tests for types

---
+++ test/clj/introspect/type_test.clj
@@ -0,0 +1,51 @@
+(ns introspect.type-test
+  (:import [introspect.helpers FooChildA FooChildB ParentClass ChildClass])
+  (:require [clojure.test :refer :all]
+            [introspect.type :refer :all]
+            [introspect.helpers.simple-functions :as h1]))
+
+(deftest have-common-ancestor-test
+  (is (have-common-ancestor? (type (FooChildA.))
+                             (type (FooChildB.))))
+  )
+
+(deftest subtype-test
+  (is (subtype? (type (ParentClass.))
+                (type (ChildClass.))))
+  (is (any-subtype? (type (ParentClass.))
+                    (type (ChildClass.))))
+  (is (any-subtype? (type (ChildClass.))
+                    (type (ParentClass.))))
+  )
+
+(deftest call-types-match-test
+  (is (call-types-match? [Integer Integer Integer]
+                         #{[Integer Integer Integer]}))
+
+  (is (not (call-types-match? [Integer Integer]
+                              #{[Integer Integer Integer]})))
+
+  (is (call-types-match? [(type (fn []))]
+                         #{[(type (fn []))]}))
+
+  (is (not (call-types-match? [(type (fn []))]
+                              #{[Integer]})))
+
+  (is (call-types-match? [(type (fn []))]
+                         #{[(type (fn []))]}))
+
+  (is (call-types-match? [(type (fn []))]
+                         #{[(type (fn []))]}))
+
+  (is (call-types-match? [(type (ParentClass.)) (type (ParentClass.))]
+                         #{[(type (ChildClass.)) (type (ChildClass.))]}
+                         ))
+
+  (is (call-types-match? [(type (ChildClass.)) (type (ChildClass.))]
+                         #{[(type (ParentClass.)) (type (ParentClass.))]}
+                         ))
+
+
+  (is (call-types-match? [(type (ParentClass.)) (type (ChildClass.))]
+                         #{[(type (ParentClass.)) (type (ChildClass.))]}
+                         )))
