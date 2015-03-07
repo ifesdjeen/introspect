b1461002dca8e0964464e884d8242718d5a812bc
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-07 20:50:49 +0100

    Improve test

--- test/clj/introspect/core_test.clj
+++ test/clj/introspect/core_test.clj
@@ -4,11 +4,13 @@
             [introspect.core :refer :all]
             [introspect.helpers.simple-functions :as h1]))
 
-(deftest a-test
+(deftest t-test
   (h1/constant-return-value-fn 1 1)
   (h1/constant-return-value-fn "1" "1")
 
-  (is (= "(java.lang.Long -> java.lang.String -> java.lang.String)
-(java.lang.Long -> java.lang.Long -> java.lang.Long)\n"
-         (with-out-str
-           (t h1/constant-return-value-fn)))))
+  (let [types (set
+               (clojure.string/split (with-out-str
+                                       (t h1/constant-return-value-fn))
+                                     #"\n"))]
+    (not (nil? (get types  "(java.lang.Long -> java.lang.Long -> java.lang.Long)")))
+    (not (nil? (get types  "(java.lang.Long -> java.lang.String -> java.lang.String)")))))
