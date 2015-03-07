a8adf45314b31c9ac1d67cde0066c9c868c2f18b
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-07 21:48:19 +0100

    Add basic protocol test

--- test/clj/introspect/helpers/simple_functions.clj
+++ test/clj/introspect/helpers/simple_functions.clj
@@ -1,5 +1,16 @@
 (ns introspect.helpers.simple-functions)
 
+
+(defprotocol SomeProtocol
+  (somemethod [this a b]))
+
+(deftype SomeType []
+  SomeProtocol
+  (somemethod [this a b]
+    2))
+
+(defn make-some-type
+  [] (SomeType.))
 (defn constant-return-value-fn
   [input-1 input-2]
   2)
