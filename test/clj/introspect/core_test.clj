e84c88d5874fad38144dcea2eaec2a97cd1627c5
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-11 23:15:23 +0100

    Bring the tests back

--- test/clj/introspect/core_test.clj
+++ test/clj/introspect/core_test.clj
@@ -4,25 +4,27 @@
             [introspect.core :refer :all]
             [testing.helpers.simple-functions :as h1]))
 
-(comment
-  (deftest t-test
-    (is (= 2 (h1/constant-return-value-fn 1 1)))
-    (is (= 2) (h1/constant-return-value-fn "1" "1"))
+(deftest t-test
+  (is (= 2 (h1/constant-return-value-fn 1 1)))
+  (is (= 2 (h1/constant-return-value-fn "1" "1")))
 
-    (let [types (set
-                 (clojure.string/split (with-out-str
-                                         (t h1/constant-return-value-fn))
-                                       #"\n"))]
-      (is (not (nil? (get types  "(java.lang.Long -> java.lang.Long -> java.lang.Long)"))))
-      (is (not (nil? (get types  "(java.lang.Long -> java.lang.String -> java.lang.String)")))))))
+  (let [types (set
+               (clojure.string/split (with-out-str
+                                       (t h1/constant-return-value-fn))
+                                     #"\n"))]
+    (is (not (nil? (get types  "(java.lang.Long -> java.lang.Long -> java.lang.Long)"))))
+    (is (not (nil? (get types  "(java.lang.String -> java.lang.String -> java.lang.Long)"))))))
 
 
 (deftest protocols-test
-  (let [t (h1/make-some-type)]
-    (println (h1/somemethod t (java.util.Date.) (java.util.Date.)))
-    (println (h1/somemethod2 t 1.121 1.0123 1.123123))
-    )
-  ;; (h1/somemethod (h1/make-some-type) 1 1)
-  ;; (println (h1/somemethod (h1/make-some-type) "1" "1"))
-  (println (get-method-calls)) ;; (t h1/somemethod)
-  )
+  (let [inst (h1/make-some-type)]
+    (is (= 2 (h1/somemethod inst (java.util.Date.) (java.util.Date.))))
+    (is (= "str2" (h1/somemethod2 inst 1.121 1.0123 1.123123)))
+
+    (is (= "(java.util.Date -> java.util.Date -> java.lang.Long)\n"
+           (with-out-str
+             (t (class inst) "somemethod"))))
+
+    (is (= "(java.lang.Double -> java.lang.Double -> java.lang.Double -> java.lang.String)\n"
+           (with-out-str
+             (t (class inst) "somemethod2"))))))
