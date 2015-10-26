2f3fc31f2e61ad2eeccaf23b1e413d60ddaeb58b
Alex Petrov <oleksandr.petrov@gmail.com>
2015-10-26 22:31:18 +0100

    Add function returns tracking

--- src/java/introspect/IntrospectProfilingAgent.java
+++ src/java/introspect/IntrospectProfilingAgent.java
@@ -20,13 +20,15 @@ public class IntrospectProfilingAgent {
 
   private static class MethodLogger extends AdviceAdapter {
 
+    private final String className;
     private final String name;
     private final String desc;
     private final int access;
 
-    protected MethodLogger(int api, MethodVisitor methodVisitor, int access, String name, String desc) {
+    protected MethodLogger(int api, String className, MethodVisitor methodVisitor, int access, String name, String desc) {
       super(api, methodVisitor, access, name, desc);
 
+      this.className = className;
       this.desc = desc;
       this.name = name;
       this.access = access;
@@ -34,42 +36,49 @@ public class IntrospectProfilingAgent {
 
     @Override
     protected void onMethodEnter() {
-      // checks if the method is static.
-      // The difference is that "this" is stored in ALOAD_0 and the arguments are stored in ALOAD_1, ALOAD_2, ...
-      // But there is no "this" for a static method call. Therefor the arguments are stored in ALOAD_0, ALOAD_1 ,...
-      // If we want to access the arguments we need to differentiate between static and non static method calls.
       boolean isStatic = (access & ACC_STATIC) > 0;
 
       int length = Type.getArgumentTypes(desc).length;
 
-      // pushes the method name on the stack
-      super.visitLdcInsn(name);
-      // pushes the count of arguments on the stack
-      // could be optimized if we would use iconst_0, iconst_1, ..., iconst_5 for 0 to 5.
       super.visitIntInsn(BIPUSH, length);
-      // creates an object array with the count of arguments
       super.visitTypeInsn(ANEWARRAY, "java/lang/Object");
 
-      // stores the arguments in the array
       for (int i = 0; i < length; i++) {
-        // duplicates the reference to the array. Because the AASTORE opcode consumes the stack element with the reference to the array.
-        super.visitInsn(DUP);
-        // could be optimized
+        dup();
         super.visitIntInsn(BIPUSH, i);
-        // puts the value of the current argument on the stack
         super.visitVarInsn(ALOAD, i + (isStatic ? 0 : 1));
-        // stores the value of the current argument in the array
         super.visitInsn(AASTORE);
       }
 
-      // calls the MethodLogger#log(String, Object...) method with the corresponding arguments - which we created just before
-      super.visitMethodInsn(INVOKESTATIC, "introspect/Interceptor", "log", "(Ljava/lang/String;[Ljava/lang/Object;)V", false);
+
+      super.visitLdcInsn(className);
+      super.visitLdcInsn(name);
+      super.visitMethodInsn(INVOKESTATIC, "introspect/Interceptor", "in", "([Ljava/lang/Object;Ljava/lang/String;Ljava/lang/String;)V", false);
     }
 
-//    @Override
-//    protected void onMethodExit(int opcode) {
-//
-//    }
+    @Override
+    protected void onMethodExit(int opcode) {
+      // pushes the method name on the stack
+
+      if(opcode==RETURN) {
+        visitInsn(ACONST_NULL);
+      } else if(opcode==ARETURN || opcode==ATHROW) {
+        dup();
+      } else {
+        if(opcode==LRETURN || opcode==DRETURN) {
+          dup2();
+        } else {
+          dup();
+        }
+        box(Type.getReturnType(this.methodDesc));
+
+      }
+
+      super.visitLdcInsn(className);
+      super.visitLdcInsn(name);
+      super.visitMethodInsn(INVOKESTATIC, "introspect/Interceptor", "out", "(Ljava/lang/Object;Ljava/lang/String;Ljava/lang/String;)V", false);
+    }
+
   }
 
 
@@ -86,18 +95,26 @@ public class IntrospectProfilingAgent {
             @Override
             public ClassVisitor wrap(ClassVisitor classVisitor) {
               return new ClassVisitor(Opcodes.ASM4, classVisitor) {
+
+                private String className;
+
+                @Override
+                public void visit(int version, int access, String className, String signature, String superName, String[] interfaces) {
+                  super.visit(version, access, className, signature, superName, interfaces);
+                  this.className = className;
+                }
+
                 @Override
                 public MethodVisitor visitMethod(int access, String name, String desc, String signature,
                                                  String[] ex) {
                   MethodVisitor orig = super.visitMethod(access, name, desc, signature, ex);
-                  System.out.println("Methodname:" + name);
-                  System.out.println("Desc:" +desc);
 
+                  System.out.println("Injecting: " + className + " " + name);
                   if (!name.equals("invoke")) {
                     return orig;
+                  } else {
+                    return new MethodLogger(Opcodes.ASM4, className, orig, access, name, desc);
                   }
-
-                  return new MethodLogger(Opcodes.ASM4, orig, access, name, desc);
                 }
               };
             }
