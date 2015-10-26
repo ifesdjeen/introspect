66595202952560c2894adcd9175f970960872da5
Alex Petrov <oleksandr.petrov@gmail.com>
2015-10-26 22:34:06 +0100

    Fix interceptor

--- src/java/introspect/Interceptor.java
+++ src/java/introspect/Interceptor.java
@@ -1,42 +1,21 @@
 package introspect;
 
 import clojure.lang.IFn;
-import net.bytebuddy.implementation.bind.annotation.*;
 
-import java.lang.reflect.Method;
-import java.util.concurrent.Callable;
+import java.util.Arrays;
 
 public class Interceptor {
 
   public static IFn callback;
 
-  public static void log(String a, Object[] obj) {
-    System.out.println("HEREEEE");
-    System.out.println(obj);
-
+  public static void in(Object[] arguments, String className, String methodName) {
+    System.out.println("Enter method: '" + className + " " + methodName + "' in:" + Arrays.toString(arguments));
+    // callback.invoke(methodName, arguments);
   }
-  // @RuntimeType
-  public static void intercept(// @SuperCall Callable method,
-                                 @Origin Method originalMethod
-//                                 @This Object proxy,
-//                                 @AllArguments Object[] allArguments
-                                ) // throws Throwable
-  {
-//    Object returnValue = null;
-//    try {
-//      System.out.println("CALLING");
-//      returnValue = method.call();
-//    } catch (Throwable throwable) {
-//      callback.invoke(proxy, allArguments, throwable);
-//      throwable.printStackTrace();
-//      throw throwable;
-//    }
-//
-//    // System.out.printf("___%s, returnValue: %s___\n",proxy, returnValue);
-//    callback.invoke(proxy, originalMethod, allArguments, returnValue);
-//    return returnValue;
-    System.out.println("HERE WE GO");
-    System.out.println(originalMethod);
+
+  public static void out(Object argument, String className, String methodName) {
+    System.out.println("Leave method: '" + className + " " + methodName + "' in:" + argument);
+        // callback.invoke(methodName, arguments);
   }
 
   public static void setCallback(IFn fn) {
