8ce83e8b9df0a99c3b18b32b75a14d9e619b0b70
Alex Petrov <oleksandr.petrov@gmail.com>
2015-10-27 18:09:24 +0100

    Add stack tracing to introspection

--- src/java/introspect/Interceptor.java
+++ src/java/introspect/Interceptor.java
@@ -1,25 +1,54 @@
 package introspect;
 
-import clojure.lang.IFn;
-
 import java.util.Arrays;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import clojure.lang.IFn;
 
 public class Interceptor {
 
-  public static IFn callback;
+  public static IFn                               callback;
+  public static ConcurrentLinkedDeque<MethodCall> argumentStack = new ConcurrentLinkedDeque<>();
 
   public static void in(Object[] arguments, String className, String methodName) {
-    System.out.println("Enter method: '" + className + " " + methodName + "' in:" + Arrays.toString(arguments));
-    // callback.invoke(methodName, arguments);
+    argumentStack.push(new MethodCall(className, methodName, arguments));
   }
 
-  public static void out(Object argument, String className, String methodName) {
-    System.out.println("Leave method: '" + className + " " + methodName + "' in:" + argument);
-        // callback.invoke(methodName, arguments);
+  public static void out(Object returnArg, String className, String methodName) {
+		MethodCall call = argumentStack.pop();
+		assert(call.getClassName().equals(className));
+		assert(call.getMethodName().equals(methodName));
+
+		callback.invoke(call.getClassName(), call.getMethodName(), call.getArguments(), returnArg);
   }
 
   public static void setCallback(IFn fn) {
     callback = fn;
   }
 
+  public static class MethodCall {
+
+    private final String   className;
+    private final String   methodName;
+    private final Object[] arguments;
+
+    public MethodCall(String className, String methodName, Object[] arguments) {
+      this.className = className;
+      this.methodName = methodName;
+      this.arguments = arguments;
+    }
+
+    public String getClassName() {
+      return className;
+    }
+
+    public String getMethodName() {
+      return methodName;
+    }
+
+    public Object[] getArguments() {
+      return arguments;
+    }
+
+  }
 }
